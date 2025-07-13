b60ed779f1649cca39470d1b630f6ab8797945a7
Nikita Savelev <pybepod@gmail.com>
2025-07-13 13:00:46 +0300

    Add src/hooks/useNavigationObserver.tsx

---
+++ src/hooks/useNavigationObserver.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+//import { useContext } from "react";
+//import { NavigationObserverContext } from "@/components/NavigationObserver";
+
+const useNavigationObserver = () => {
+  /*
+  const ctx = useContext(NavigationObserverContext);
+
+  if (!ctx) {
+    console.error("NavigationObserverContext used outside of provider!");
+    throw new Error("NavigationObserverContext must be used within its Provider");
+  }
+
+  return ctx?.activeSection || "";
+  */
+
+  return "";
+};
+
+export default useNavigationObserver;
