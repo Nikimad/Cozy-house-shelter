32a6da801b4281957e7751585324b68c1f813cf1
Nikita Savelev <pybepod@gmail.com>
2025-07-13 13:01:13 +0300

    Change src/app/layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,20 @@
-import { FC } from "react";
+import type { FC, ReactNode } from "react";
 
 import type { Metadata } from "next";
-import { Geist } from "next/font/google";
+import { Roboto } from "next/font/google";
+import cn from "classnames";
+
+import Providers from "@/components/Providers";
+import Header from "./_components/Header";
+
+import "@/styles/global.scss";
 
 interface RootLayoutProps {
-  children: React.ReactNode;
+  children: ReactNode;
 }
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const roboto = Roboto({
+  variable: "--roboto",
   subsets: ["latin"],
 });
 
@@ -20,7 +26,12 @@ export const metadata: Metadata = {
 const RootLayout: FC<RootLayoutProps> = ({ children }) => {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable}`}>{children}</body>
+      <body className={cn("body", roboto.className)}>
+        <Providers>
+          <Header />
+          {children}
+        </Providers>
+      </body>
     </html>
   );
 };
