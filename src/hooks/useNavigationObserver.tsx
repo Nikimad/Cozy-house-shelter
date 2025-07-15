9b76d79b9d17fcb3b396f5f595e35eedc2d01261
Nikita Savelev <pybepod@gmail.com>
2025-07-15 13:21:49 +0300

    Fix src/hooks/useNavigationObserver.tsx: remove comments

--- src/hooks/useNavigationObserver.tsx
+++ src/hooks/useNavigationObserver.tsx
@@ -1,21 +1,12 @@
 "use client";
 
-//import { useContext } from "react";
-//import { NavigationObserverContext } from "@/components/NavigationObserver";
+import { useContext } from "react";
+import { NavigationObserverContext } from "@/components/NavigationObserver";
 
 const useNavigationObserver = () => {
-  /*
   const ctx = useContext(NavigationObserverContext);
 
-  if (!ctx) {
-    console.error("NavigationObserverContext used outside of provider!");
-    throw new Error("NavigationObserverContext must be used within its Provider");
-  }
-
   return ctx?.activeSection || "";
-  */
-
-  return "";
 };
 
 export default useNavigationObserver;
