85a64fdda18739abeb78f0ce08cad663d1461601
Nikita Savelev <91181114+Nikimad@users.noreply.github.com>
2025-09-30 17:03:00 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import Help from "./_sections/help/Help";
 
 const Home = () => (
   <>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/4.3.7/iframeResizer.min.js"></script>
+<iframe onload="iFrameResize({ log: false, checkOrigin: false })" src="https://2ca6c9d19ad94bfc9105c1e4c89fc4cc.elf.site" style="border: none; width: 100%" allow="fullscreen"></iframe>
     <Banner />
     <About />
     <Help />
